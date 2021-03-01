143c28b01225db087af5c7e1eca5d2e24c8e2f5d
George <leonardogeorge95@gmail.com>
2021-03-01 22:38:00 +0200

    First Task

--- src/com/virtualpairprogrammers/services/customers/CustomerManagementImpl.java
+++ src/com/virtualpairprogrammers/services/customers/CustomerManagementImpl.java
@@ -1,5 +1,79 @@
 package com.virtualpairprogrammers.services.customers;
 
-public class CustomerManagementImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.virtualpairprogrammers.domain.Call;
+import com.virtualpairprogrammers.domain.Customer;
+
+public class CustomerManagementImpl implements CustomerManagementService{
+	private List<Customer> customerList;
+	
+	public CustomerManagementImpl() {
+		this.customerList = new ArrayList<Customer>();
+	}
+
+	@Override
+	public void newCustomer(Customer newCustomer) {
+		this.customerList.add(newCustomer);
+		
+	}
+
+	@Override
+	public void updateCustomer(Customer changedCustomer) {
+		for(Customer c : customerList) {
+			if(c.getCustomerId().equals(changedCustomer.getCustomerId()))
+				c = changedCustomer;
+		}
+		
+	}
+
+	@Override
+	public void deleteCustomer(Customer oldCustomer) {
+		this.customerList.remove(oldCustomer);
+		
+	}
+
+	@Override
+	public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
+		for(Customer c : customerList) {
+			if(c.getCustomerId().equals(customerId))
+				return c;
+		}
+		return null;
+	}
+
+	@Override
+	public List<Customer> findCustomersByName(String name) {
+		List<Customer> rCustomers = new ArrayList();
+		for(Customer c : customerList) {
+			if(c.getCompanyName().equals(name))
+				rCustomers.add(c);
+		}
+		return rCustomers;
+	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		return this.customerList;
+	}
+
+	@Override
+	public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
+		for(Customer c : customerList) {
+			if(c.getCustomerId().equals(customerId))
+				return c;
+		}
+		return null;
+	}
+
+	@Override
+	public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
+		for(Customer c : customerList) {
+			if(c.getCustomerId().equals(customerId))
+				c.addCall(callDetails);
+		}
+		
+	}
 
 }
--- src/com/virtualpairprogrammers/services/diary/DiaryManagementImpl.java
+++ src/com/virtualpairprogrammers/services/diary/DiaryManagementImpl.java
@@ -1,5 +1,31 @@
 package com.virtualpairprogrammers.services.diary;
 
-public class DiaryManagementImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.virtualpairprogrammers.domain.Action;
+
+public class DiaryManagementImpl implements DiaryManagementService{
+	private List<Action> actionList;
+	
+	public DiaryManagementImpl() {
+		this.actionList = new ArrayList<Action>();
+	}
+	
+	@Override
+	public void recordAction(Action action) {
+		this.actionList.add(action);
+		
+	}
+
+	@Override
+	public List<Action> getAllIncompleteActions(String requiredUser) {
+		List<Action> incompleteActions = new ArrayList<Action>();
+		for(Action action : actionList) {
+			if(action.getOwningUser().equals(requiredUser) && !action.isComplete())
+				incompleteActions.add(action);
+		}
+		return incompleteActions;
+	}
 
 }
